1d27b3d43e9bc499c547abc460c234f990f5ccb8
Bangly <stachecki.piotr@gmail.com>
2023-03-27 18:19:55 +0200

    first commit

---
+++ src/main/java/pl/ziwg/medialibrex/MediaLibrexApplication.java
@@ -0,0 +1,13 @@
+package pl.ziwg.medialibrex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaLibrexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaLibrexApplication.class, args);
+    }
+
+}
