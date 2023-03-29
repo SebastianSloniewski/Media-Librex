f41a134376b582aef6ac35b6bcbd311e9007617d
Bangly <stachecki.piotr@gmail.com>
2023-03-29 15:27:36 +0200

    first commit

---
+++ src/test/java/pl/ziwg/medialibrex/MediaLibrexApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ziwg.medialibrex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaLibrexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
