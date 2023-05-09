30a5de02ba68536678fe6ad00297a2f4eab4f9e5
Metran10 <grzetra@op.pl>
2023-05-09 13:08:29 +0200

    Skonfigurowanie CORS dla kontrolerów

--- src/main/java/pl/ziwg/medialibrex/API/MusicAPI/MusicApiController.java
+++ src/main/java/pl/ziwg/medialibrex/API/MusicAPI/MusicApiController.java
@@ -2,12 +2,10 @@ package pl.ziwg.medialibrex.API.MusicAPI;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class MusicApiController {
     private final MusicApiService musicApiService;
